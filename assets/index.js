10c6929d284b5363e426d552998188155bdc7ee2
Melissa Kouadio <angemelisk@gmail.com>
2022-12-05 14:48:18 +0000

    update

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,15 @@
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
+  let toTop = document.getElementById("toTop");
+  // add button got to top
+  window.onscroll = function() {scrollFunction()};
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      toTop.style.display = "block";
+    } else {
+      toTop.style.display = "none";
+    }
+  }
+  // Image galerie full width
   for (let i of document.querySelectorAll('.gallery img')) {
     i.onclick = () => i.classList.toggle("full");
   }
